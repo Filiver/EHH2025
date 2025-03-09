0813846acfddab4bb858cd839ac92d4c3eb0c79e
majklost <majkl.mrkos@seznam.cz>
2025-03-09 09:02:01 +0100

    html outline

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,7 +1,10 @@
 "use strict";
 
+// const fetchedData = showMessage();
+document.addEventListener("DOMContentLoaded", async () => {});
+
 async function showMessage() {
   const data = await fetch("/api/data");
   const jsonData = await data.json();
-  console.log(jsonData);
+  return jsonData;
 }
