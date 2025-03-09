4660fe9d7b8e8aebfe97927be3ad65ed5d7b7c26
majklost <majkl.mrkos@seznam.cz>
2025-03-09 11:15:08 +0100

    next

--- CKD/src/backend/model.py
+++ CKD/src/backend/model.py
@@ -8,13 +8,15 @@ from numpy.ma.extras import average
 
 from constants import *
 
+
 class MeasurementError(ValueError):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
+
 class Alert:
-    def __init__(self, message, source, severity = 3):
+    def __init__(self, message, source, severity=3):
         """
         :param message: Text of the alert
         :param severity: 1 - low, 5 - high
@@ -38,10 +40,14 @@ class Patient:
         self.sex, self.dob, self.age = self.get_patient_info()
 
         self.egfr_date, self.egfr, self.egfr_unit, self.egfr_note, self.average_egfr, self.all_egfr, self.dates_egfr = self.get_egfr()
-        self.uacr_date, self.uacr, self.uacr_unit, self.uacr_note, self.average_uacr, self.all_uacr, self.dates_uacr = self.get_lab("UACR")
-        self.pu_date, self.pu, self.pu_unit, self.pu_note, self.average_pu, self.all_pu, self.dates_pu = self.get_lab("PU")
-        self.upcr_date, self.upcr, self.upcr_unit, self.upcr_note, self.average_upcr, self.all_upcr, self.dates_upcr = self.get_lab("UPCR")
-        self.s_kreatinin_date, self.s_kreatinin, self.s_kreatinin_unit, self.s_kreatinin_note, self.average_s_kreatinin, self.all_s_kreatinin, self.dates_s_kreatinin = self.get_lab("s_kreatinin")
+        self.uacr_date, self.uacr, self.uacr_unit, self.uacr_note, self.average_uacr, self.all_uacr, self.dates_uacr = self.get_lab(
+            "UACR")
+        self.pu_date, self.pu, self.pu_unit, self.pu_note, self.average_pu, self.all_pu, self.dates_pu = self.get_lab(
+            "PU")
+        self.upcr_date, self.upcr, self.upcr_unit, self.upcr_note, self.average_upcr, self.all_upcr, self.dates_upcr = self.get_lab(
+            "UPCR")
+        self.s_kreatinin_date, self.s_kreatinin, self.s_kreatinin_unit, self.s_kreatinin_note, self.average_s_kreatinin, self.all_s_kreatinin, self.dates_s_kreatinin = self.get_lab(
+            "s_kreatinin")
 
         self.transplants = self.get_transplants()
         self.diagnoses = self.get_diagnoses()
@@ -78,7 +84,6 @@ class Patient:
         clf = load_model("RF_labs_diagnoses_optimised.pkl")
         pred = clf.predict([self.to_row()])
 
-
     def generate_alerts(self):
         # alerts = []
         # if self.ckd_stage == 3 and (self.date - self.last_nefrology_visit).days >= LAST_NEFROLOGY_VISIT_SILENCER:
@@ -105,8 +110,9 @@ class Patient:
         odber = []
         messages = []
 
-        nefrology_silencer = (self.date - self.last_nefrology_visit).days >= LAST_NEFROLOGY_VISIT_SILENCER
-        
+        nefrology_silencer = (
+            self.date - self.last_nefrology_visit).days >= LAST_NEFROLOGY_VISIT_SILENCER
+
         if self.ckd_stage is not None:
             if self.ckd_stage != 0:
                 if nefrology_silencer:
@@ -136,15 +142,16 @@ class Patient:
                 else:
                     if (self.date - self.uacr_date).days >= 360:
                         odber.append("UACR")
-        
+
         sub_diagnoses = self.diagnoses[:-2].astype(int)
         if sub_diagnoses.sum() != 0:
             score += 10 * sub_diagnoses.sum()
             sources.append("DIAGNOSES")
-            messages.append(f"Rizikové faktory: {', '.join([DIAGNOSES[diag] for diag in np.nonzero(sub_diagnoses)[0]])}")
+            messages.append(
+                f"Rizikové faktory: {', '.join([DIAGNOSES[diag] for diag in np.nonzero(sub_diagnoses)[0]])}")
         if len(odber) > 0:
             messages.append(f"Doporučeno vyšetření {', '.join(odber)}")
-        
+
         severity = min(score, 100) // 25 + 1
         messages.insert(0, f"{SEVERITIES[severity - 1]} riziko")
         if severity >= 3 and nefrology_silencer:
@@ -248,7 +255,6 @@ class Patient:
                 return 2
         return None
 
-
     def get_lab(self, analyte, date=None, period=None):
         date = date if date is not None else self.date
         period = period if period is not None else self.period
@@ -265,13 +271,15 @@ class Patient:
         values = []
         if len(rows) == 0:
             return None, None, None, None, None, [], []
-        last_measurement_date = datetime.datetime.strptime(rows[0][0], "%Y-%m-%d").date()
+        last_measurement_date = datetime.datetime.strptime(
+            rows[0][0], "%Y-%m-%d").date()
         minimum_date = last_measurement_date - period
         values_sum = 0
         values_count = 0
         values_dates = []
         for row in rows:
-            current_date = datetime.datetime.strptime(row[0], "%Y-%m-%d").date()
+            current_date = datetime.datetime.strptime(
+                row[0], "%Y-%m-%d").date()
             if current_date < minimum_date:
                 break
             if row is None or row[2] is None or row[4] != TYPICAL_UNIT[analyte]:
@@ -322,7 +330,6 @@ class Patient:
             sex = None
         return sex, dob, age
 
-
     def get_egfr(self, date=None, period=None):
         date = date if date is not None else self.date
         period = period if period is not None else self.period
@@ -339,7 +346,8 @@ class Patient:
 
         if len(rows) != 0:
             row = rows[0]
-            last_measurement_date = datetime.datetime.strptime(row[0], "%Y-%m-%d").date()
+            last_measurement_date = datetime.datetime.strptime(
+                row[0], "%Y-%m-%d").date()
             minimum_date = last_measurement_date - period
 
             if row[2] is None or (row[4] != "ml/s/1,73 m2" and row[4] != "ml/s/spt"):
@@ -354,7 +362,8 @@ class Patient:
             all_egfr = []
             values_dates = []
             for r in rows:
-                current_date = datetime.datetime.strptime(r[0], "%Y-%m-%d").date()
+                current_date = datetime.datetime.strptime(
+                    r[0], "%Y-%m-%d").date()
                 if current_date < minimum_date:
                     break
                 if r[2] is None or (row[4] != "ml/s/1,73 m2" and row[4] != "ml/s/spt"):
@@ -385,7 +394,8 @@ class Patient:
             return None, None, None, None, None, [], []
 
         values = []
-        last_measurement_date = datetime.datetime.strptime(rows[0][0], "%Y-%m-%d").date()
+        last_measurement_date = datetime.datetime.strptime(
+            rows[0][0], "%Y-%m-%d").date()
         minimum_date = last_measurement_date - period
 
         values_sum = 0
@@ -393,16 +403,17 @@ class Patient:
         values_dates = []
 
         for row in rows:
-            current_date = datetime.datetime.strptime(row[0], "%Y-%m-%d").date()
+            current_date = datetime.datetime.strptime(
+                row[0], "%Y-%m-%d").date()
             if current_date < minimum_date:
                 break
             if self.sex is not None and self.age is not None and row[2] is not None and \
-                row[4] == 'µmol/l':
+                    row[4] == 'µmol/l':
                 s_kreatinin = umol_l_to_mg_dl(row[2])
                 values.append(
                     round(EGFR_COEF[self.sex][0] * min(s_kreatinin / EGFR_BOUNDARY[self.sex], 1) ** EGFR_COEF[self.sex][1]
-                      * max(s_kreatinin / EGFR_BOUNDARY[self.sex], 1) ** (EGFR_COEF[self.sex][2]) *
-                      (0.993 ** self.age), 1))
+                          * max(s_kreatinin / EGFR_BOUNDARY[self.sex], 1) ** (EGFR_COEF[self.sex][2]) *
+                          (0.993 ** self.age), 1))
                 values_sum += values[-1]
                 values_count += 1
                 values_dates.append(current_date)
@@ -420,10 +431,63 @@ class Patient:
         values_average = values_sum / values_count if values_count != 0 else None
         return last_measurement_date, value, "ml/min/1,73 m2", note, values_average, values, values_dates
 
+    def toJSON(self):
+        return {"patient_id": self.patient_id,
+                "date": self.date.strftime('%Y-%m-%d'),
+                "period": str(self.period),
+                "dob": self.dob.strftime('%Y-%m-%d') if self.dob is not None else None,
+                "age": self.age,
+                "sex": self.sex,
+                "egfr_date": self.egfr_date.strftime('%Y-%m-%d') if self.egfr_date is not None else None,
+                "egfr": self.egfr,
+                "egfr_unit": self.egfr_unit,
+                "egfr_note": self.egfr_note,
+                "average_egfr": self.average_egfr,
+                "all_egfr": self.all_egfr,
+                "uacr_date": self.uacr_date.strftime('%Y-%m-%d') if self.uacr_date is not None else None,
+                "uacr": self.uacr,
+                "uacr_unit": self.uacr_unit,
+                "uacr_note": self.uacr_note,
+                "average_uacr": self.average_uacr,
+                "all_uacr": self.all_uacr,
+                "pu_date": self.pu_date.strftime('%Y-%m-%d') if self.pu_date is not None else None,
+                "pu": self.pu,
+                "pu_unit": self.pu_unit,
+                "pu_note": self.pu_note,
+                "average_pu": self.average_pu,
+                "all_pu": self.all_pu,
+                "upcr_date": self.upcr_date.strftime('%Y-%m-%d') if self.upcr_date is not None else None,
+                "upcr": self.upcr,
+                "upcr_unit": self.upcr_unit,
+                "upcr_note": self.upcr_note,
+                "average_upcr": self.average_upcr,
+                "all_upcr": self.all_upcr,
+                "s_kreatinin_date": self.s_kreatinin_date.strftime('%Y-%m-%d') if self.s_kreatinin_date is not None else None,
+                "s_kreatinin": self.s_kreatinin,
+                "s_kreatinin_unit": self.s_kreatinin_unit,
+                "s_kreatinin_note": self.s_kreatinin_note,
+                "average_s_kreatinin": self.average_s_kreatinin,
+                "all_s_kreatinin": self.all_s_kreatinin,
+                "transplants": self.transplants.tolist(),
+                "gfr_category": self.gfr_category,
+                "uacr_category": self.uacr_category,
+                "ckd_stage": self.ckd_stage,
+                "last_nefrology_visit": self.last_nefrology_visit.strftime('%Y-%m-%d') if self.last_nefrology_visit is not None else None,
+                "in_nefrology_care": self.in_nefrology_care,
+                "alerts": [alert.toJSON() for alert in self.alerts]}
+
+
 DB = "../../data/CKD_train.db"
+
+
+def change_db(new_db):
+    global DB
+    DB = new_db
+
 
 def mg_dl_to_umol_l(mg_dl):
     return mg_dl / KREATININ_MOLAR_MASS * 10000
+
 
 def umol_l_to_mg_dl(umol_l):
     return umol_l * KREATININ_MOLAR_MASS / 10000
@@ -431,7 +495,8 @@ def umol_l_to_mg_dl(umol_l):
 
 if __name__ == '__main__':
     # patient = Patient(1215860, datetime.date(year=2025, month=1, day=1), period=datetime.timedelta(days=5000))
-    patient = Patient(840, datetime.date(year=2021, month=1, day=1), risk_assesment=True)
+    patient = Patient(840, datetime.date(
+        year=2021, month=1, day=1), risk_assesment=True)
     # patient = Patient(4030, datetime.date(year=2021, month=1, day=1))
     print(patient.__dict__)
     print(patient.alerts)
